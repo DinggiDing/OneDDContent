a549994c3a6d4766f9d4a2d5740888e8dd55b182
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-07-24 19:54:14 +0900

    24.07.24 style 2 add

---
+++ Sources/OneDDContent/InDiary/InD_SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 성재 on 7/24/24.
+//
+
+import SwiftUI
+
+struct InD_SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InD_SwiftUIView2()
+}
